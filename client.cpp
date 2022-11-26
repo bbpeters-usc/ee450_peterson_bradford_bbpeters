71a9beb4ea9b70879da599839033715f5662dc51
Bradford Peterson <bbpeters@usc.edu>
2022-11-25 18:26:24 -0800

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -8,21 +8,22 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <iostream>
-#define PORT 25633 // the port client will be connecting to
-#define MAXDATASIZE 50 // max number of bytes we can get at once
+
+#define SERVERM_PORT 25633 // the port client will be connecting to
+#define MAXDATASIZE 51 // max number of bytes we can get at once
 
 using namespace std;
 
 int main() {
-	int sockfd, numbytes;
+	int serverM_fd, numbytes;
 	char buf[MAXDATASIZE];
 	struct hostent *he;
-	struct sockaddr_in their_addr; // connector’s address information
+	struct sockaddr_in serverM_addr; 
 	if ((he=gethostbyname("localhost")) == NULL) { // get the host info
 		herror("gethostbyname");
 		exit(1);
 	}
-	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+	if ((serverM_fd = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
 		perror("socket");
 		exit(1);
 	}
@@ -39,32 +40,33 @@ int main() {
 	temp = password;
 	temp.append(50-temp.length(), '\0'); 
 
-	their_addr.sin_family = AF_INET; // host byte order
-	their_addr.sin_port = htons(PORT); // short, network byte order
-	their_addr.sin_addr = *((struct in_addr *)he->h_addr);
-	memset(&(their_addr.sin_zero), '\0', 8); // zero the rest of the struct
-	if (connect(sockfd, (struct sockaddr *)&their_addr, sizeof(struct sockaddr)) == -1) {
+	serverM_addr.sin_family = AF_INET; // host byte order
+	serverM_addr.sin_port = htons(SERVERM_PORT); // short, network byte order
+	serverM_addr.sin_addr = *((struct in_addr *)he->h_addr);
+	memset(&(serverM_addr.sin_zero), '\0', 8); // zero the rest of the struct
+	if (connect(serverM_fd, (struct sockaddr *)&serverM_addr, sizeof(struct sockaddr)) == -1) {
 		perror("connect");
 		exit(1);
 	}
 	string msg = username;
-        msg.append(50-msg.length(), '\0');
-	if (send(sockfd, msg.c_str() , 50, 0) == -1) {
+    msg.append(50-msg.length(), '\0');
+	if (send(serverM_fd, msg.c_str() , 50, 0) == -1) {
                                 perror("send");
                         }
 	msg = password;
-        msg.append(50-msg.length(), '\0');
-	if (send(sockfd, msg.c_str() , 50, 0) == -1) {
+    msg.append(50-msg.length(), '\0');
+	if (send(serverM_fd, msg.c_str() , 50, 0) == -1) {
                                 perror("send");
                         }
 	cout << username << " sent an authentication request to the main server."<< endl;
-	if ((numbytes=recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+
+	if ((numbytes=recv(serverM_fd, buf, MAXDATASIZE-1, 0)) == -1) {
 		perror("recv");
 		exit(1);
 	}
 	buf[numbytes] = '\0';
 	printf("Received: %s",buf);
-	close(sockfd);
+	close(serverM_fd);
 	return 0;
 }
 
