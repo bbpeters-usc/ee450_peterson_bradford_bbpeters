883d6c17ef4d1e791bcdf2a6ec517a8d00085757
Bradford Peterson <bbpeters@usc.edu>
2022-11-28 02:40:19 -0800

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -19,6 +19,9 @@ int main() {
 	char buf[MAXDATASIZE];
 	struct hostent *he;
 	struct sockaddr_in serverM_addr; 
+	struct sockaddr_in client_addr;
+	unsigned int client_port;
+
 	if ((he=gethostbyname("localhost")) == NULL) { // get the host info
 		herror("gethostbyname");
 		exit(1);
@@ -27,19 +30,6 @@ int main() {
 		perror("socket");
 		exit(1);
 	}
-	cout << "The client is up and running" << endl;
-
-	string username, password;
-	cout << "Please enter the username: ";
-	getline(cin, username);
-	cout << "Please enter the password: ";
-	getline(cin, password);
-	
-	string temp = username;
-	temp.append(50-temp.length(), '\0');
-	temp = password;
-	temp.append(50-temp.length(), '\0'); 
-
 	serverM_addr.sin_family = AF_INET; // host byte order
 	serverM_addr.sin_port = htons(SERVERM_PORT); // short, network byte order
 	serverM_addr.sin_addr = *((struct in_addr *)he->h_addr);
@@ -48,25 +38,85 @@ int main() {
 		perror("connect");
 		exit(1);
 	}
-	string msg = username;
-    msg.append(50-msg.length(), '\0');
-	if (send(serverM_fd, msg.c_str() , 50, 0) == -1) {
-                                perror("send");
-                        }
-	msg = password;
-    msg.append(50-msg.length(), '\0');
-	if (send(serverM_fd, msg.c_str() , 50, 0) == -1) {
-                                perror("send");
-                        }
-	cout << username << " sent an authentication request to the main server."<< endl;
+	//Retrieve the locally-bound name of the specified socket and store it in the sockaddr structure
+	bzero(&client_addr, sizeof(client_addr));
+	socklen_t len = sizeof(client_addr);
+	getsockname(client_port, (struct sockaddr *) &client_addr, &len);
+	client_port = ntohs(client_addr.sin_port);
 
-	if ((numbytes=recv(serverM_fd, buf, MAXDATASIZE-1, 0)) == -1) {
-		perror("recv");
-		exit(1);
+	cout << "The client is up and running" << endl;
+
+	string username, password;
+	int n = 2;
+	while(1){ //Authentication loop
+		cout << "Please enter the username: ";
+		getline(cin, username);
+		cout << "Please enter the password: ";
+		getline(cin, password);
+
+		string msg = username;
+		msg.append(50-msg.length(), '\0');
+		if (send(serverM_fd, msg.c_str() , 50, 0) == -1) {
+			perror("send");
+		}
+		msg = password;
+		msg.append(50-msg.length(), '\0');
+		if (send(serverM_fd, msg.c_str() , 50, 0) == -1) {
+			perror("send");
+		}
+		cout << username << " sent an authentication request to the main server." << endl;
+		
+		if ((numbytes=recv(serverM_fd, buf, 1, 0)) == -1) {
+			perror("recv");
+			exit(1);
+		}
+		buf[numbytes] = '\0';
+
+		if(buf[0] == '2'){ 
+			cout << username << " received the result of authentication using TCP over port " << client_port << ". "; 
+			cout << "Authentication is successful" << endl;
+			break; 
+		} else if(n == -1){
+			cout << "Authentication Failed for 3 attempts. Client will shut down." << endl;
+			exit(0);
+		} else if(buf[0] == '0'){
+			cout << username << " received the result of authentication using TCP over port " << client_port << ". "; 
+			cout << "Authentication failed: Username Does not exist" << endl;
+			cout << "Attempts remaining: " << n << endl;
+			n--
+		} else {
+			cout << username << " received the result of authentication using TCP over port " << client_port << ". "; 
+			cout << "Authentication failed: Password does not match" << endl;
+			cout << "Attempts remaining: " << n << endl;
+			n--
+		}
 	}
-	buf[numbytes] = '\0';
-	printf("Received: %s",buf);
+	
+	string course, category;
+	while(1){ //query loop
+		cout << "Please enter the course code to query: ";
+		getline(cin, course);
+		cout << "Please enter the category (Credit / Professor / Days / CourseName): ";
+		getline(cin, category);
+		while((category != "Credit") || (category != "Professor") || (category != "Days") || (category != "CourseName")){
+			cout << "Error. " << category << " is not a valid category." << endl;
+			cout << "Please enter the category (Credit / Professor / Days / CourseName): ";
+			getline(cin, category);
+		}
+
+
+		if (send(serverM_fd, course.c_str() , 5, 0) == -1) {
+			perror("send");
+		}
+
+		category.erase(2, category.length()-1);
+		if (send(serverM_fd, category.c_str() , 2, 0) == -1) {
+			perror("send");
+		}
+		cout << username << " sent a request to the main server." << endl;
+
+	}
+	
 	close(serverM_fd);
 	return 0;
 }
-
